0bf527dc2c4cc61ed2485c5abaf3c25f8c699b2b
Dudko-Mikhail <demon37525@mail.ru>
2021-05-22 22:04:59 +0300

    added functionality and improved design

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Calculator
 //
-//  Created by Student on 6.05.21.
+//  Created by Dudko Mikhail on 6.05.21.
 //
 
 import SwiftUI
@@ -23,7 +23,94 @@ struct ContentView: View {
         ["colorLightGray", "colorLightGray", "colorLightGray", "colorOrange"]
     ]
     
-    var result = "0"
+    @State var result = "0"
+    @State var isOperation = false
+    @State var operation: String = ""
+    @State var firstOperand: Double = 0
+    @State var secondOperand: Double = 0
+    func btnClick(text: String) {
+        switch text {
+            case "0"..."9":
+                digitButtonClicked(text)
+            case "AC", "MR":
+                result = "0"
+                firstOperand = 0
+                secondOperand = 0
+                operation = ""
+            case "%":
+                result = String((Double(result) ?? 0) / 100)
+                isOperation = false
+            case "+/-":
+                if !Double(result)!.isZero {
+                    if (result[result.startIndex] != "-") {
+                        result = "-" + result
+                    }
+                    else {
+                        result.removeFirst()
+                    }
+                }
+            case ",":
+                if !result.contains(".") {
+                    result += "."
+                }
+            case "=":
+                if operation != "" {
+                    if isOperation == false {
+                        secondOperand = Double(result) ?? 0
+                    }
+                    executeOperation(operation)
+                    isOperation = true
+
+                }
+                print("=")
+            default:
+                if operation == "" {
+                    firstOperand = Double(result) ?? 0
+                }
+                else {
+                    if !isOperation {
+                        secondOperand = Double(result) ?? 0
+                        executeOperation(operation)
+                    }
+                }
+                isOperation = true
+                operation = text
+        }
+    }
+    
+    func digitButtonClicked(_ text: String) {
+        if result == "0" || isOperation {
+            result = text
+            isOperation = false
+        }
+        else if (result.count < 11) {
+            result += text
+        }
+        
+    }
+    
+    func executeOperation(_ operation: String) {
+//        print("first: \(self.firstOperand)")
+//        print("second: \(self.secondOperand)")
+        switch operation {
+            case "+":
+                firstOperand += secondOperand
+            case "-":
+                firstOperand -= secondOperand
+            case "/":
+                firstOperand /= secondOperand
+            case "x":
+                firstOperand *= secondOperand
+            default:
+                print("impossible")
+        }
+        if firstOperand.isEqual(to: Double(Int(firstOperand))) {
+            result = String(Int(firstOperand))
+        }
+        else {
+            result = String(firstOperand)
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 2) {
@@ -33,23 +120,35 @@ struct ContentView: View {
                     .font(.system(size: 32))
                     .foregroundColor(Color("colorWhite"))
                     .padding(15)
+                    .padding()
+                    
             }
             ForEach(0..<5) { row in
-                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 2) {
+                HStack(alignment: .center, spacing: 2) {
                     ForEach(0..<4) { col in
-                        Button(action: {}) {
-                            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/) {
-                                Text(btnText[row][col])
-                            }
+                        Button(action: {self.btnClick(text: self.btnText[row][col])}) {
+                            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
+                                Spacer()
+                                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
+                                    Spacer()
+                                    Text(btnText[row][col])
+                                        .foregroundColor(Color("colorBlack"))
+                                    Spacer()
+                                        
+                                }
+                                Spacer()
+                             }
                             .background(Color(btnColorNames[row][col]))
                             .cornerRadius(5)
+                            .font(.system(size: 22))
+
                         }
-                        Spacer()
                     }
                 }
             }
-            .background(Color("colorBlack"))
         }
+        .background(Color("colorBlack"))
+        
     }
 }
 
